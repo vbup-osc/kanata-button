01ce04b22566114b8b9b65c3ac754a51fed7a363
Cyame <marlonxu1121@qq.com>
2021-05-19 07:06:25 +0000

    Deploying to gh-pages from  @ ea8f96b367a6f1454222a4496172ec34036eb9dd 🚀

--- _nuxt/static/1621401940/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fkanata-button\u002F_nuxt\u002Fstatic\u002F1621401940",layout:"default",error:a,state:{locales:["en","ja",b],locale:b,dark:false},serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fkanata-button\u002F",assetsPath:"\u002Fkanata-button\u002F_nuxt\u002F",cdnURL:a}}}}(null,"zh"));
